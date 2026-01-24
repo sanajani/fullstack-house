df67c44d503bc87daf9e3c8a1e33d6c040bb573c
Sanaullah <webdevfarsi@gmail.com>
2026-01-24 10:04:44 -0800

    registration done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,22 @@
+import AppError from '../errors/AppError.js';
+import { registerUserService } from '../services/userServices.js';
 import { asyncErrorHandler } from '../utils/asyncErrorHandler.js';
 import { registerationSchemaValidation } from '../validations/registerationJoiValidation.js';
 
 
-export const registerUser = (req,res) => {
+export const registerUser = asyncErrorHandler(async (req,res, next) => {
     const userData = req.body;
+
     // Logic to handle user registration using userData
     const { error } = registerationSchemaValidation.validate(userData);
+    if( error ) {
+        return next(new AppError(error.details[0].message, 400));
+    }
 
+    const user = await registerUserService(userData);
+    if(!user) {
+        return next(new AppError('User registration failed', 500));
+    }
 
-    
-};
+    res.status(201).json({ message: 'User registered successfully', data: user });
+});
--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,3 +1,23 @@
-export const registerUserService = () => {
+import AppError from "../errors/AppError.js";
+import UserModel from "../models/UserModel.js";
+
+import bcrypt from 'bcryptjs';
+
+export const registerUserService = async (userData) => {
     // Business logic for registering a user would go here
+    const isUserExists = await UserModel.findOne({
+        $or: [
+            { email: userData.email }, 
+            { username: userData.username }, 
+            { phoneNumber1: userData.phoneNumber1 }
+        ]
+    })
+    if(isUserExists) throw new AppError('User already exist', 409);
+
+    userData.password = await bcrypt.hash(userData.password, 12);
+
+
+    const newUser = new UserModel(userData);
+    return await newUser.save();
+
 };

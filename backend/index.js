9ef93035a9b945209c39bf96a9c33b99f5374ff8
Sanaullah <webdevfarsi@gmail.com>
2026-01-25 12:19:45 -0800

    security section

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,47 @@
 import 'dotenv/config';
+
+import limit from 'express-rate-limit';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import ExpressMongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+
 import express from 'express';
 import connectToMongoDB from './db/mongodbConnection.js';
 import cors from 'cors';
 import customError from './errors/customError.js';
 
 const app = express();
+
+const limiter = limit({
+    windowMs: 45 * 60 * 1000, // 45 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 45 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const loginLimit = limit({
+    windowMs: 9 * 60 * 1000, // 9 minutes
+    max: 10, // Limit each IP to 100 requests per `window` (here, per 9 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(helmet());
+app.use('/api/v1', limiter);
+app.use('/api/v1/users/login', loginLimit);
+app.use(hpp());
+
 const PORT = process.env.PORT || 5000;
 
 // import  UserRoute
 import UserRoute from './routes/UserRoute.js';
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10kb'}));
+
+app.use(ExpressMongoSanitize());
+app.use(xss());
 
 // Use UserRoute for user-related endpoints
 app.use('/api/v1/users', UserRoute);
